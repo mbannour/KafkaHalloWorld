ba69b7a8e12bd36a7b8f9d78e1cf6c76f6cc3bab
Mohamed Ali Bannour <>
2018-04-26 15:03:57 +0200

    fixed issue.

--- src/main/scala/com/dali/producer/KafkaProducerApp.scala
+++ src/main/scala/com/dali/producer/KafkaProducerApp.scala
@@ -15,10 +15,10 @@ class KafkaProducerApp {
   var producer: Producer[String, String] = new KafkaProducer[String, String](properties)
 
   def send(): Unit =
-    try
-      (1 to 5).foreach ( i =>
+    try {
+      (1 to 5).foreach(i =>
         producer.send(new ProducerRecord[String, String]("my-topic", i.toString, "MyMessage: " + i.toString)))
-    catch {
+    } catch {
       case e: Exception => println(e)
     } finally
       producer.close()
